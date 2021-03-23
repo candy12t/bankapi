fcb2d796a4ce950f65e535147f52a113cd98c4b0
Takumi Kanada <candy12t@gmail.com>
2021-03-23 15:38:55 +0900

    feat: Account

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/candy12t/bank"
 )
 
+var accounts = map[float64]*bank.Account{}
+
 func main() {
-	fmt.Println(bank.Hello())
+	accounts[1001] = &bank.Account{
+		Customer: bank.Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number: 1001,
+	}
 }
