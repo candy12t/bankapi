aae722cefd14c7403117df56668891449fc19a9b
Takumi Kanada <candy12t@gmail.com>
2021-03-24 15:44:51 +0900

    feat: transfer

--- main.go
+++ main.go
@@ -21,9 +21,19 @@ func main() {
 		Number: 1001,
 	}
 
+	accounts[1002] = &bank.Account{
+		Customer: bank.Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number: 1002,
+	}
+
 	http.HandleFunc("/statement", statement)
 	http.HandleFunc("/deposit", deposit)
 	http.HandleFunc("/withdraw", withdraw)
+	http.HandleFunc("/transfer", transfer)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -100,6 +110,38 @@ func withdraw(w http.ResponseWriter, req *http.Request) {
 				fmt.Fprintf(w, "%v", err)
 			} else {
 				fmt.Fprintf(w, account.Statement())
+			}
+		}
+	}
+}
+
+func transfer(w http.ResponseWriter, req *http.Request) {
+	numberqs := req.URL.Query().Get("number")
+	amountqs := req.URL.Query().Get("amount")
+	destqs := req.URL.Query().Get("dest")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
+
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if dest, err := strconv.ParseFloat(destqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account destination number!")
+	} else {
+		if accountA, ok := accounts[number]; !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else if accountB, ok := accounts[dest]; !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", dest)
+		} else {
+			err := accountA.Transfer(amount, accountB)
+			if err != nil {
+				fmt.Fprintf(w, "%v", err)
+			} else {
+				fmt.Fprintf(w, accountA.Statement())
 			}
 		}
 	}
